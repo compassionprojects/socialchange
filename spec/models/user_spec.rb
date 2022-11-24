0ad2452f7db44f41caa5daad33dfc03e44ec84fa
madhums <me@madhums.me>
2022-11-24 13:26:55 +0100

    set validations for language attribute + tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe User do
-  describe 'validations' do
-    context 'when name is empty' do
-      subject { create(:user) }
+  describe "validations" do
+    describe "name" do
+      subject { create(:user, email: "test@example.com", password: "12345678") }
 
-      it 'should not create a new user and raise validation errors' do
+      it "should not create a new user and raise validation errors" do
         expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+        expect { subject }.to raise_error(/name can't be blank/i)
+        expect(User.count).to be 0
+      end
+    end
+
+    describe "language" do
+      subject { create(:user, language: :fr, name: "Test", email: "test@example.com", password: "12345678") }
+
+      it "should not create a new user and raise validation errors" do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+        expect { subject }.to raise_error(/language is not included in the list/i)
         expect(User.count).to be 0
       end
     end
