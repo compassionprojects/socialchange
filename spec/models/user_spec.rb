efb348345984ace27524e3a6e85df27a62646b44
madhums <me@madhums.me>
2022-11-26 14:10:04 +0100

    add more user model tests. closes #15

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,30 @@ require "rails_helper"
 
 describe User do
   describe "validations" do
+    describe "email" do
+      subject { create(:user, email: "test", password: "12345678", name: "abc") }
+
+      it "should be valid" do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+        expect { subject }.to raise_error(/Email address is invalid/i)
+        expect(User.count).to be 0
+      end
+    end
+
+    describe "password" do
+      subject { create(:user, email: "test@example.com", password: "1234567", name: "abc") }
+
+      it "should be atleast 8 characters" do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+        expect { subject }.to raise_error(/Password is too short/i)
+        expect(User.count).to be 0
+      end
+    end
+
     describe "name" do
       subject { create(:user, email: "test@example.com", password: "12345678") }
 
-      it "should not create a new user and raise validation errors" do
+      it "should validate presence of name" do
         expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
         expect { subject }.to raise_error(/name can't be blank/i)
         expect(User.count).to be 0
@@ -15,10 +35,19 @@ describe User do
     describe "language" do
       subject { create(:user, language: :fr, name: "Test", email: "test@example.com", password: "12345678") }
 
-      it "should not create a new user and raise validation errors" do
+      it "should validate language" do
         expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
         expect { subject }.to raise_error(/language is not included in the list/i)
         expect(User.count).to be 0
+      end
+    end
+
+    context "when valid" do
+      subject { create(:user, name: "Test", email: "test@example.com", password: "12345678") }
+
+      it "should create a user" do
+        expect { subject }.not_to raise_error
+        expect(User.count).to be 1
       end
     end
   end
