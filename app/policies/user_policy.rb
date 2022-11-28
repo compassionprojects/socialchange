79156da382a1c32d136e6d49d6fab4d6dc36c4a2
madhums <me@madhums.me>
2022-11-28 11:16:43 +0100

    update tests. closes #16

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,10 +1,10 @@
 class UserPolicy < ApplicationPolicy
 
-  # Allow only current users to update their profile
+  # Allow only current users and site admins to update users profile
   # Devise too, manages this and only allows updating of current_user profile
   # But we have added this as a failsafe
   #
   def update?
-    user.id == record.id
+    user.admin? || user.id == record.id
   end
 end
