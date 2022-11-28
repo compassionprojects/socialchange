00d43eb53add7700697d08e99263fe8734b0e37d
madhums <me@madhums.me>
2022-11-28 19:58:56 +0100

    update code formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :users
-
-      root to: "users#index"
-    end
-  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
 
   # Defines the root path route ("/")
   root "home#index"
