2dd8944828499a4d0935a2b308ba2540cf6259ac
madhums <me@madhums.me>
2022-12-11 21:37:41 +0100

    add stories views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: "users/registrations" }
 
+  resources :stories
+
   namespace :admin do
     resources :users
     resources :roles
