ceea48d461d90914a1ef6df925392d14bf3cce53
madhums <me@madhums.me>
2022-12-11 20:54:12 +0100

    fix db seed

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,5 +6,15 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8) }
+
+    factory :user_with_stories do
+      transient do
+        stories_count { 5 }
+      end
+
+      stories do
+        Array.new(stories_count) { association(:story, user: instance, updater: instance) }
+      end
+    end
   end
 end
