9603bd209c3f0aa0d009a5687eb47bca9d0b45e7
madhums <me@madhums.me>
2022-12-11 19:24:26 +0100

    make sure edit form doesn't contain password attribute

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -85,10 +85,16 @@ class UserDashboard < Administrate::BaseDashboard
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = %i[
+  FORM_ATTRIBUTES_NEW = %i[
     name
     email
     password
+    language
+  ].freeze
+
+  FORM_ATTRIBUTES = %i[
+    name
+    email
     language
   ].freeze
 
