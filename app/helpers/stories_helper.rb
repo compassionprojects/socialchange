14623175538acae9db8bb09581b231beef172230
madhums <me@madhums.me>
2022-12-13 13:36:32 +0100

    fix rubocop offense by not using instance variables in helpers

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,11 +1,11 @@
 # Stories helper
 #
 module StoriesHelper
-  def can_edit?
-    user_signed_in? && policy(@story).update?
+  def can_edit?(story)
+    user_signed_in? && policy(story).update?
   end
 
-  def can_destroy?
-    user_signed_in? && policy(@story).destroy?
+  def can_destroy?(story)
+    user_signed_in? && policy(story).destroy?
   end
 end
