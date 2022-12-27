81b22859e68b2ee102384dc61a0f248aa76af6db
madhums <me@madhums.me>
2022-12-27 16:38:57 +0200

    use double quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def time_ago(time)
-    time_ago_in_words(time, scope: 'datetime.distance_in_words.short')
+    time_ago_in_words(time, scope: "datetime.distance_in_words.short")
   end
 end
