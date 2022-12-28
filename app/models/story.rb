c7fe472b6133a061129cea436a72c39c539a6985
madhums <me@madhums.me>
2022-12-28 14:44:51 +0200

    add inverse_of and fix offense also note that using inverse_of helps us with memory optimisation a bit

--- app/models/story.rb
+++ app/models/story.rb
@@ -6,7 +6,7 @@ class Story < ApplicationRecord
 
   belongs_to :user
   belongs_to :updater, class_name: "User"
-  has_many :story_updates, -> { kept.order(created_at: :asc) }, dependent: :destroy
+  has_many :story_updates, -> { kept.order(created_at: :asc) }, dependent: :destroy, inverse_of: :story
 
   enum :status, %i[draft published]
 
