1d5cbc6e2654bef00b0586f2cf4fb0db97ba0068
madhums <me@madhums.me>
2022-12-28 12:37:04 +0200

    use bootstrap 5.3.0-alpha and add dark mode support

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,21 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+// Set auto color mode
+// https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
+
+const getPreferredTheme = () => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = function (theme) {
+  if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+  }
+}
+
+setTheme(getPreferredTheme())
