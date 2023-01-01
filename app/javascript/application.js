1ffa6e730178aa5cb126318f3167e4ed8efafd72
madhums <me@madhums.me>
2023-01-01 20:30:29 +0200

    include direct_uploads and trix css/js files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "./direct_uploads"
 import * as bootstrap from "bootstrap"
 import * as ActiveStorage from "@rails/activestorage"
 import "trix"
