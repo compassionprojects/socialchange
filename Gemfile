a2fc7d60a30874deeef5f4685dd6ccce434923ad
madhums <me@madhums.me>
2023-01-01 18:11:46 +0200

    fix rubocop offenses

--- Gemfile
+++ Gemfile
@@ -102,5 +102,5 @@ end
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-gem "image_processing", ">= 1.2"
 gem "aws-sdk-s3", require: false
+gem "image_processing", ">= 1.2"
--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -7,10 +7,10 @@ Rails.configuration.to_prepare do
 
     def generate_key_with_prefix
       self.key = if prefix
-        File.join prefix, self.class.generate_unique_secure_token
-      else
-        self.class.generate_unique_secure_token
-      end
+                   File.join prefix, self.class.generate_unique_secure_token
+                 else
+                   self.class.generate_unique_secure_token
+                 end
     end
 
     def prefix
