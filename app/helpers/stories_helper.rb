c655100ab58cd0248351df87a7f4bcc3aa08db41
madhums <me@madhums.me>
2023-01-02 12:22:19 +0200

    sanitize rich text display

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -8,4 +8,8 @@ module StoriesHelper
   def can_destroy?(story)
     user_signed_in? && policy(story).destroy?
   end
+
+  def display_rich_text(text)
+    sanitize(text, tags: %w(strong em b i p sub sup cite br ul ol li blockquote a))
+  end
 end
