6757a4326ebdd75340dd34d85ef50ee74846ff1a
madhums <me@madhums.me>
2023-02-04 11:58:53 +0100

    use dark and light logos for light and dark themes resp.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,26 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "./direct_uploads"
-import * as bootstrap from "bootstrap"
-import * as ActiveStorage from "@rails/activestorage"
-import "trix"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "./direct_uploads";
+import * as bootstrap from "bootstrap";
+import * as ActiveStorage from "@rails/activestorage";
+import "trix";
 
-ActiveStorage.start()
+ActiveStorage.start();
 
 // Set auto color mode
 // https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
 
+const inverted = {
+  dark: 'light',
+  light: 'dark'
+};
+
 const getPreferredTheme = () => {
   return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-}
+};
 
 const setTheme = function (theme) {
   if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute('data-bs-theme', 'dark')
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
   } else {
-    document.documentElement.setAttribute('data-bs-theme', theme)
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    document.getElementById('logo').setAttribute('src', `/logo-${inverted[theme]}.png`);
   }
-}
+};
 
-setTheme(getPreferredTheme())
+setTheme(getPreferredTheme());
