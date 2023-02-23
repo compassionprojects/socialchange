4a5363812f8133069f8b4ed89b97a6c3ad1d403d
madhums <me@madhums.me>
2023-02-23 12:19:21 +0100

    view related changes and add translations

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -15,7 +15,7 @@ class DiscussionsController < ApplicationController
 
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to story_discussion_url(@discussion.story, @discussion) }
+        format.html { redirect_to story_discussion_url(@discussion.story, @discussion), notice: I18n.t("discussions.created") }
         format.json { render :show, status: :created, location: @discussion }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +41,7 @@ class DiscussionsController < ApplicationController
 
     respond_to do |format|
       if @discussion.update(**permitted_attributes(@discussion), updater: current_user)
-        format.html { redirect_to story_discussion_url(@discussion.story, @discussion) }
+        format.html { redirect_to story_discussion_url(@discussion.story, @discussion), notice: I18n.t("discussions.updated") }
         format.json { render :show, status: :ok, location: @discussion }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,7 +55,7 @@ class DiscussionsController < ApplicationController
     @discussion.discard
 
     respond_to do |format|
-      format.html { redirect_to story_discussions_url(@discussion.story), notice: I18n.t("discussion.deleted") }
+      format.html { redirect_to story_discussions_url(@discussion.story), notice: I18n.t("discussions.deleted") }
       format.json { render status: :no_content }
     end
   end
