d97842100990b66cca4b0a07d9538d7ecf89a000
madhums <me@madhums.me>
2023-02-24 10:47:42 +0100

    add missing has_many association for discussions and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_and_belongs_to_many :roles
 
   has_many :stories, dependent: :destroy
+  has_many :discussions, dependent: :destroy
+  has_many :posts, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
