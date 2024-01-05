deb4a08c75348b86e1d9c90c9f75e33bb51864a1
madhums <me@madhums.me>
2024-01-06 00:38:13 +0100

    use ApplicationMailer for devise emails so that we have the same layout for all emails

---
+++ spec/mailers/previews/devise_preview.rb
@@ -0,0 +1,26 @@
+# Devise emails can be previewed here
+# http://localhost:3000/rails/mailers/devise_mailer/[method_name].html
+
+class DeviseMailerPreview < ActionMailer::Preview
+  # We do not have confirmable enabled, but if we did, this is
+  # how we could generate a preview:
+  def confirmation_instructions
+    Devise::Mailer.confirmation_instructions(User.first, "faketoken")
+  end
+
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.first, "faketoken")
+  end
+
+  def unlock_instructions
+    Devise::Mailer.unlock_instructions(User.first, "faketoken")
+  end
+
+  def email_changed
+    Devise::Mailer.email_changed(User.first)
+  end
+
+  def password_changed
+    Devise::Mailer.password_change(User.first)
+  end
+end
