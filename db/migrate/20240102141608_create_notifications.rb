c4f68c87a0946c9878640c395d96b9c1ad9ca78c
madhums <me@madhums.me>
2024-01-27 23:50:12 +0100

    create table and index only if it does not exist

--- db/migrate/20240102141608_create_notifications.rb
+++ db/migrate/20240102141608_create_notifications.rb
@@ -1,6 +1,6 @@
 class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
-    create_table :notifications do |t|
+    create_table :notifications, if_not_exists: true do |t|
       t.references :recipient, polymorphic: true, null: false
       t.string :type, null: false
       t.jsonb :params
@@ -8,6 +8,6 @@ class CreateNotifications < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-    add_index :notifications, :read_at
+    add_index :notifications, :read_at, if_not_exists: true
   end
 end
