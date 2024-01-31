8de20746e6674b5d7593cdda00ad871fd340077a
madhums <me@madhums.me>
2024-01-31 11:25:06 +0100

    always show navigation progress bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
+import { Turbo } from "@hotwired/turbo-rails";
 import "./controllers";
 import "./direct_uploads";
 import * as bootstrap from "bootstrap";
 import * as ActiveStorage from "@rails/activestorage";
 import "trix";
+
+// always show progress bar
+Turbo.setProgressBarDelay(0);
 
 ActiveStorage.start();
 
