b1c2d24b5961e1437b74267949e2f59408360593
madhums <me@madhums.me>
2024-02-01 19:43:57 +0100

    make sure category name is unique

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :category do
-    name { Faker::Book.genre }
+    # since name is unique, make sure we don't get a duplicate
+    sequence(:name) { |n| Faker::Book.genre + n.to_s }
   end
 end
