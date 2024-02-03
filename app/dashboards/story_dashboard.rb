5765d0c9cfe78200df490001ff0c95aab132c396
madhums <me@madhums.me>
2024-02-03 10:48:28 +0100

    display category below title

--- app/dashboards/story_dashboard.rb
+++ app/dashboards/story_dashboard.rb
@@ -63,8 +63,8 @@ class StoryDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    category
     title
+    category
     description
     outcomes
     source
