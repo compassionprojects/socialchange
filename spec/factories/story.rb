a0f1e1cca86b8a6c625c3dd66af0c91dda4868eb
madhums <me@madhums.me>
2024-02-05 12:31:27 +0100

    add category selection for a story and tests

--- spec/factories/story.rb
+++ spec/factories/story.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :story do
     title { Faker::Lorem.sentence }
-    description { Faker::Lorem.paragraph(random_sentences_to_add: 30) }
-    outcomes { Faker::Lorem.paragraph(random_sentences_to_add: 30) }
-    source { Faker::Lorem.paragraph(random_sentences_to_add: 4) }
+    description { Faker::Lorem.paragraph(random_sentences_to_add: 2) }
+    outcomes { Faker::Lorem.paragraph(random_sentences_to_add: 2) }
+    source { Faker::Lorem.paragraph(random_sentences_to_add: 2) }
     country { Faker::Address.country_code }
     category
     user
