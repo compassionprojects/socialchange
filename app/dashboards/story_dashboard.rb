c1395df8b1510723f02c196cb116729db653d0fd
madhums <me@madhums.me>
2024-02-14 10:40:01 +0100

    remove story status from admin

--- app/dashboards/story_dashboard.rb
+++ app/dashboards/story_dashboard.rb
@@ -60,7 +60,6 @@ class StoryDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    status
     title
     description
     outcomes
