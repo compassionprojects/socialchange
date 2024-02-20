25e7c257113f59809e7da1251e1a1af0422393cd
madhums <me@madhums.me>
2024-02-20 11:10:22 +0100

    move admin related assets out of the app

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,4 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link application.js
-//= link administrate/application.js
-//= link admin.js
 //= link application.css
-//= link administrate/application.css
-//= link admin.css
