8ebfe2620135d6e75dfa18b13335d246d14e8fda
madhums <me@madhums.me>
2024-02-28 14:03:09 +0100

    remove noticed initializer

--- config/initializers/noticed.rb
+++
@@ -1,8 +0,0 @@
-module Noticed
-  # @todo default_url_options not available in Notifier::Notification
-  # https://github.com/excid3/noticed/discussions/404#discussioncomment-8549378
-  #
-  # module NotificationMethods
-  #   include Rails.application.routes.url_helpers
-  # end
-end
