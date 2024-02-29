814cf07b1ef4fafb1c782ea9ebc617a7f5b59c77
madhums <me@madhums.me>
2024-02-29 20:10:35 +0100

    remove outdated code comments

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,7 +2,7 @@ class Contribution < ApplicationRecord
   belongs_to :user
   belongs_to :story, touch: true
 
-  after_create_commit :invite_contributor, unless: -> { Rails.env.test? }
+  after_create_commit :invite_contributor
 
   private
 
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -12,13 +12,6 @@ class Discussion < ApplicationRecord
 
   validates :title, :description, presence: true
 
-  # @todo remove the unless filter
-  # figure out why tests get stuck and thorw errors like these
-  # https://github.com/compassionprojects/socialchange/actions/runs/7434566400/job/20228849583?pr=57
-  # WARNING:  there is already a transaction in progress
-  # ActiveRecord::StatementInvalid:
-  #   PG::UnableToSend: insufficient data in "T" message
-  #
   after_create_commit :notify
 
   # After a topic is discarded, discard it's posts
