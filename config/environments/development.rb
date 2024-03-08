a319150c097baa76821e496d39b3c64119de9304
madhums <me@madhums.me>
2024-03-08 13:16:22 +0100

    move to tigris, away from digitalocean. closes #142

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_controller.default_url_options = routes.default_url_options = {lang: I18n.locale, host: ENV["DOMAIN_NAME"]}
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :digitalocean
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
