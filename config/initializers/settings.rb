e945a30c2fd86aea1bfdc970cb6df2c40e322318
madhums <me@madhums.me>
2024-03-12 13:06:23 +0100

    add code comments and bring back the singleton Using singleton avoids initialisation of the Settings class (which is mostly global)

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,7 +1,17 @@
+# The Settings class is a singleton class which holds all configuration
+# which is accessed throughout the application outside of `config` files.
+# These settings mostly include environemt variables and other global settings.
+#
+# Similar pattern and inspiration from:
+# https://garrettdimon.com/journal/posts/unified-configuration-in-rails
+#
+# Usage: Settings.instance.contact_email
+#
+
 class Settings
-  class << self
-    def contact_email
-      ENV["DOMAIN_EMAIL_ADDRESS"]
-    end
+  include Singleton
+
+  def contact_email
+    ENV["DOMAIN_EMAIL_ADDRESS"]
   end
 end
