b7d11f9360e5b3b2e97f0d6e44560d4159da5d16
madhums <me@madhums.me>
2024-03-16 13:52:52 +0100

    don't chain encrypt method as it returns nil. closes #158

--- db/migrate/20240316113449_encrypt_email_user_attribute.rb
+++ db/migrate/20240316113449_encrypt_email_user_attribute.rb
@@ -1,9 +1,15 @@
 class EncryptEmailUserAttribute < ActiveRecord::Migration[7.1]
   def up
-    User.all.find_each { |u| u.encrypt.save }
+    User.all.find_each do |u|
+      u.encrypt
+      u.save
+    end
   end
 
   def down
-    User.all.find_each { |u| u.decrypt.save }
+    User.all.find_each do |u|
+      u.decrypt
+      u.save
+    end
   end
 end
